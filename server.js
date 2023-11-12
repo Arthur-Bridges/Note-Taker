f59736cd7fe7c3f7a46e0d708151e8b0ee539ef3
Arthur Bridges <arthurbridges08@yahoo.com>
2023-11-12 16:48:18 -0600

    debug

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/api', api);
 
+
 // GET/FETCH data
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
